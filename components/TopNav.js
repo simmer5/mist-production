a31e115580da81fcd6e6b210354b5c4982e28777
Mindaugas <strumke@yahoo.com>
2020-12-07 23:52:47 +0100

    respnsivnes logo

---
+++ components/TopNav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Logo from './logos/BlueLogo'
+
+const TopNav = () => {
+	return (
+		<>
+			<Logo />
+			<ul>
+				<li>Home</li>
+				<li>Puslapis</li>
+				<li>Puslapis</li>
+			</ul>
+		</>
+	)
+}
+
+export default TopNav
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.scss'
 import { AnimatePresence } from 'framer-motion'
+import Logo from '../components/logos/BlueLogo'
 
 function MyApp({ Component, pageProps, router }) {
 	return (
 		<>
 			<AnimatePresence exitBeforeEnter>
+				<Logo />
 				<Component {...pageProps} />
 			</AnimatePresence>
 		</>
